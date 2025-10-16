ad5d51ab94c939072804799f9235af144372fa38
Ryota-111 <taismryotasis@gmail.com>
2025-10-17 01:43:55 +0900

    Initial Commit

---
+++ Gourmet Search/Gourmet_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gourmet_SearchApp.swift
+//  Gourmet Search
+//
+//  Created by Ryota Fujitsuka on 2025/10/17.
+//
+
+import SwiftUI
+
+@main
+struct Gourmet_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Gourmet SearchTests/Gourmet_SearchTests.swift
@@ -0,0 +1,17 @@
+//
+//  Gourmet_SearchTests.swift
+//  Gourmet SearchTests
+//
+//  Created by Ryota Fujitsuka on 2025/10/17.
+//
+
+import Testing
+@testable import Gourmet_Search
+
+struct Gourmet_SearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
